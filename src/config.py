35c6a381131e938ec90a652e192a6747d1fcad7b
Leonid_MacBook <alforfon@gmail.com>
2024-07-07 13:57:35 +0300

    refactor: replace tokens to .env file

--- src/config.py
+++ src/config.py
@@ -1,2 +1,14 @@
-BOT_TOKEN = ""
-PAYMENT_TOKEN = ""
+import os
+from dotenv import load_dotenv
+from dataclasses import dataclass
+
+load_dotenv()
+@dataclass
+class Config:
+    BOT_TOKEN: str = os.getenv('BOT_TOKEN')
+    LOG_LEVEL: str = os.getenv('LOG_LEVEL', 'INFO')
+
+
+
+
+
