ff04c40e444fc41af78d0a4217a2f9b920e1c320
Leonid_MacBook <alforfon@gmail.com>
2024-08-09 14:41:45 +0300

    feat: add DBUser model

--- bot/services/database/models/user.py
+++ bot/services/database/models/user.py
@@ -1,30 +1,15 @@
-from typing import Optional
+from dataclasses import dataclass
 
 
+@dataclass
 class DBUser:
-    def __init__(self,
-                 user_id: int,
-                 tg_id: int,
-                 tg_teg: str | None,
-                 name: str,
-                 tribe_id: int,
-                 role_id: int,
-                 wallet_token: int,
-                 language: str,
-                 description: str | None = None,
-                 photo_path: str | None = None) -> None:
-        self.user_id = user_id
-        self.tg_id = tg_id
-        self.tg_teg = tg_teg
-        self.name = name
-        self.tribe_id = tribe_id
-        self.role_id = role_id
-        self.wallet_token = wallet_token
-        self.language = language
-        self.description = description
-        self.photo_path = photo_path
-
-    def __repr__(self):
-        return (f"DBUser(user_id={self.user_id}, tg_id={self.tg_id}, tg_teg='{self.tg_teg}', name='{self.name}', "
-                f"tribe_id={self.tribe_id}, role_id={self.role_id}, wallet_token={self.wallet_token}, "
-                f"language='{self.language}', description='{self.description}', photo_path='{self.photo_path}')")
+    user_id: int
+    tg_id: int
+    tg_teg: str | None
+    name: str
+    tribe_id: int
+    role_id: int
+    wallet_token: int
+    language: str
+    description: str | None
+    photo_path: str | None
